bcbd9f06f82b10985f297c103826048d5d55aa5d
gordosaurio <63026665+gordosaurio@users.noreply.github.com>
2025-02-17 00:14:34 -0500

    update

--- Aben/docker/init.sql
+++ Aben/docker/init.sql
@@ -15,13 +15,13 @@ CREATE TABLE IF NOT EXISTS tasks (
 
 -- Insertar 10 registros de ejemplo
 INSERT INTO tasks (title, description, status) VALUES
-('Tarea 1', 'Descripción de la tarea 1', 1),
-('Tarea 2', 'Descripción de la tarea 2', 0),
-('Tarea 3', 'Descripción de la tarea 3', 1),
-('Tarea 4', 'Descripción de la tarea 4', 0),
-('Tarea 5', 'Descripción de la tarea 5', 1),
-('Tarea 6', 'Descripción de la tarea 6', 0),
-('Tarea 7', 'Descripción de la tarea 7', 1),
-('Tarea 8', 'Descripción de la tarea 8', 0),
-('Tarea 9', 'Descripción de la tarea 9', 1),
-('Tarea 10', 'Descripción de la tarea 10', 0);
+('Tarea 1', 'Descripcion de la tarea 1', 1),
+('Tarea 2', 'Descripcion de la tarea 2', 0),
+('Tarea 3', 'Descripcion de la tarea 3', 1),
+('Tarea 4', 'Descripcion de la tarea 4', 0),
+('Tarea 5', 'Descripcion de la tarea 5', 1),
+('Tarea 6', 'Descripcion de la tarea 6', 0),
+('Tarea 7', 'Descripcion de la tarea 7', 1),
+('Tarea 8', 'Descripcion de la tarea 8', 0),
+('Tarea 9', 'Descripcion de la tarea 9', 1),
+('Tarea 10', 'Descripcion de la tarea 10', 0);
